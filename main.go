c4efdacaf3d048cafe5712a89dae37639c2244d5
Jeremy Thomas <jeremy.dh.thomas@gmail.com>
2023-09-19 16:34:00 -0700

    Traverse all actors for hardcoded imdb show

--- main.go
+++ main.go
@@ -8,18 +8,21 @@ import (
 
 func main() {
 	c := colly.NewCollector(
-		colly.AllowedDomains("go-colly.org"),
+		colly.AllowedDomains("imdb.com", "www.imdb.com"),
 	)
 
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		c.Visit(e.Request.AbsoluteURL(link))
+	c.OnHTML("table.cast_list > tbody", func(e *colly.HTMLElement) {
+		e.ForEach("tr", func(_ int, el *colly.HTMLElement) {
+			el.ForEach("td.primary_photo", func(_ int, el2 *colly.HTMLElement) {
+				actorUrl := el2.ChildAttr("a[href]", "href")
+				c.Visit(e.Request.AbsoluteURL(actorUrl))
+			})
+		})
 	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	c.Visit("https://go-colly.org/docs/")
+	c.Visit("https://www.imdb.com/title/tt11737520/fullcredits")
 }
